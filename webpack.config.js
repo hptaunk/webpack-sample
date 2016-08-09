09cc2d343f064f3bfa3a72633905f370fb94b08c
Taunk <himanshu.taunk@hpe.com>
2016-08-09 16:08:31 +0530

    INIT version

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+'use strick';
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    devtool: debug ? "inline-sourcemap" : null,
+    extry: "./js/script.js",
+    output: {
+        path: __dirname + '/js',
+        filename: 'script.min.js'
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
+    ]
+}
